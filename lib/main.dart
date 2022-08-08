6e67680b7f454ac6b6508927497bdc131f8dbce7
ethanlee <seungwoonlee90@gmail.com>
2022-08-08 22:02:51 +0900

    box design

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: Text("Hello"),
+        appBar: AppBar(title: Text("App"),),
+        body: Container(
+          width: double.infinity,
+          height: 50,
+          margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              border: Border.all(color: Colors.black)
+          ),
+        ),
         bottomNavigationBar: BottomAppBar(
-          child :Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page),
-            ],
+          child :SizedBox(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+            ),
           )
         ),
       ),
