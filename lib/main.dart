5c54be4fad4f006cf4d79aa36f0692bf1addbad9
ethanlee <seungwoonlee90@gmail.com>
2022-08-09 14:32:25 +0900

    setting layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.menu),
-          title: Text("App"),),
+          leading: Icon(Icons.menu, color:Colors.black),
+          title: Text("금호동 3가",
+            style:TextStyle(color: Colors.black),),
+          backgroundColor: Colors.white,
+          shadowColor: Colors.transparent,
+        ),
         body: Container(
           width: double.infinity,
-          height: 50,
+          height: 200,
           margin: EdgeInsets.all(20),
           decoration: BoxDecoration(
               border: Border.all(color: Colors.black)
@@ -25,13 +29,41 @@ class MyApp extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Button",
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.w700
+                Image.network('https://mdl.artvee.com/ft/12140po.jpg'),
+                Container(
+                  width: 150,
+                  margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                     Text("캐논 DSLR 100D",
+                       style: TextStyle(
+                           color: Colors.blue,
+                           fontWeight: FontWeight.w700
+                       ),
+                     ),
+                     Text("성동구 행당동",
+                       style: TextStyle(
+                           color: Colors.blue,
+                           fontWeight: FontWeight.w700
+                       ),
+                     ),
+                     Text("210,000원",
+                       style: TextStyle(
+                           color: Colors.blue,
+                           fontWeight: FontWeight.w700
+                       ),
+                     ),
+                     Row(
+                       mainAxisAlignment: MainAxisAlignment.end,
+                       children: [
+                         Icon(Icons.favorite_border),
+                         Text("4")
+                       ],
+                     )
+                   ],
                   ),
                 ),
-                ElevatedButton(onPressed: (){}, child: Icon(Icons.add))
               ],
             ),
         ),
